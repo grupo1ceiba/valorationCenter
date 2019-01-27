a2d0a29e90914e13204a9b9ec180a6a98e350ea4
Jesus Lozano <jalozanot2@gmail.com>
2019-01-26 21:04:04 -0500

    creando interface
    interface controller para el mockup

--- back/src/main/java/com/grupoOneValoration/valoration_group_one/controller/MockupController.java
+++ back/src/main/java/com/grupoOneValoration/valoration_group_one/controller/MockupController.java
@@ -1,15 +1,18 @@
-package com.grupoOneValoration.valoration_group_one;
+package com.grupoOneValoration.valoration_group_one.controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.grupoOneValoration.valoration_group_one.controller.impl.ImplMockupController;
+
 @RestController
 @RequestMapping("/introduction")
 @CrossOrigin("*")
-public class Controller {
+public class MockupController implements ImplMockupController{
 
+	@Override
 	@RequestMapping(value="/ping", method=RequestMethod.GET)
 	public String ping(){
 		
