a3f16a1ee4b8d7d106b6a1b8c9a696f4b912914d
Lenin Guerrero Ramírez <faulk65@gmail.com>
2019-01-27 13:55:37 -0500

    Se crean métodos para las validaciones del estrato y el calculo del seguro

--- back/src/main/java/com/valoracion/webService/ControllerWebService.java
+++ back/src/main/java/com/valoracion/webService/ControllerWebService.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.valoracion.controller.impl.ControllerStratumDirectionImpl;
 import com.valoracion.controller.impl.ControllerCalculateInsuranceImpl;
 import com.valoracion.dto.DTOProperty;
 import com.valoracion.dto.DTOValue;
@@ -22,11 +23,37 @@ public class ControllerWebService {
 	@RequestMapping(value="/valorationCenterInformationWS", method=RequestMethod.POST)
 	public Object valorationCenterInformationWS(@RequestBody DTOProperty property) throws Exception{
 		
-		DTOValue valueInsurance = new DTOValue();
-		ControllerCalculateInsuranceImpl calculateInsuranceImpl = new ControllerCalculateInsuranceImpl();
-		valueInsurance.setAmount(calculateInsuranceImpl.calculateInsuranceToPay(property.getValue().getAmount()));
-		valueInsurance.setCurrency(property.getValue().getCurrency());
+		boolean stratumOK = false;
+		DTOValue valueInsurance = null;
+		if(null!=property){
+		
+			stratumOK = this.verifyStratum(property);
+			
+			if(stratumOK){
+				
+				valueInsurance = new DTOValue();
+				valueInsurance.setAmount(this.calculateInsurence(property));
+				valueInsurance.setCurrency(property.getValue().getCurrency());
+			}
+		}
 		
 		return valueInsurance;
 	}	
+	
+	private Double calculateInsurence(DTOProperty property) throws Exception{
+		
+		Double amountInsurance = (double) 0;
+		ControllerCalculateInsuranceImpl calculateInsuranceImpl = new ControllerCalculateInsuranceImpl();
+		amountInsurance = calculateInsuranceImpl.calculateInsuranceToPay(property.getValue().getAmount());
+		
+		return amountInsurance;		
+		
+	}
+	
+	private boolean verifyStratum(DTOProperty property) throws Exception{
+		
+		ControllerStratumDirectionImpl stratumDirection = new ControllerStratumDirectionImpl();
+		
+		return stratumDirection.stratumByDirection(property.getAddress(), property.getStratum());
+	}
 }
